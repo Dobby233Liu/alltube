4e73d1fdd0da5cc2fc40a117f512dda8060bd9a5
Liu-Wen-Yuan <15816141883@163.com>
2020-03-09 18:26:15 +0800

    Update Video.php

--- classes/Video.php
+++ classes/Video.php
@@ -330,7 +330,8 @@ class Video
     {
         $process = new Process($command);
         $process->run();
-        echo($process->read(2048));
+        echo($process->getOutput());
+        echo($process->getErrorOutput());
         if($process->isSuccessful()) {echo("avconv success");} else {echo("avconv unsucessful");}
 
         return $process->isSuccessful();
